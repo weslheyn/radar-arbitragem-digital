f2d2d8298796cc1ea90531ed1445934512d0f644
weslheyn <35520050+weslheyn@users.noreply.github.com>
2026-05-02 05:20:39 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME = 'radar-arbitragem-v1';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json'];
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
-});
